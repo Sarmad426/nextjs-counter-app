e43ecf02956d04afa8097f599e27ab6932ca64e6
Sarmad rafique <sarmadrafique040@gmail.com>
2023-04-19 12:00:29 +0500

    Delete and decrement button

--- public/components/counter.tsx
+++ public/components/counter.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
 type Props = {
     value:number,
+    index:number,
+    handleIncrement:Function,
+    handleDecrement:Function,
+    handleDelete:Function,
 }
 const Counter = (counter:Props) => {
-  const {value} = counter;
-  const badgeStyles = "rounded px-3 py-2 m-3 font-medium bg-yellow-500";
+  const {value,handleIncrement,index,handleDelete,handleDecrement} = counter;
+  let badgeStyles = "rounded px-3 py-2 m-3 font-medium ";
+  badgeStyles+= value >0 ? 'bg-yellow-500' : 'bg-black text-white'
   return (
-    <div>
+    <div className="my-3 py-3">
       <span className={badgeStyles}>{value}</span>
+      <button
+        className="bg-green-500 px-3 py-2 rounded font-semibold mx-5 hover:bg-green-600"
+        onClick={() => handleIncrement(index)}
+      >
+        Increment
+      </button>
+      <button
+        className="bg-gray-300 px-3 py-2 rounded font-semibold mx-5 hover:bg-gray-400"
+        onClick={() => handleDecrement(index)}
+      >
+        Decrement
+      </button>
+      <button
+        className="bg-red-500 px-3 py-2 rounded font-semibold mx-5 hover:bg-red-600 text-white"
+        onClick={() => handleDelete(index)}
+      >
+        Delete
+      </button>
     </div>
-  )
+  );
 }
 
 export default Counter
